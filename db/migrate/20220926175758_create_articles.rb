47dbed4b5542df1347bcb0df0c9c4c171bfd1f5c
maurobiazutti <mauro_biazutti@hotmail.com>
2022-09-26 18:42:34 -0300

    commit 02

--- db/migrate/20220926175758_create_articles.rb
+++ db/migrate/20220926175758_create_articles.rb
@@ -1,7 +1,7 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
-      t.string :titulo
+      t.string :title
       t.text :body
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_03_132312) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_26_175758) do
   create_table "articles", force: :cascade do |t|
-    t.string "titulo"
+    t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
